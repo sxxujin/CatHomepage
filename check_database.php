be2ac85fba5aae2f3d0fd3cd79bbd044bdbb5eda
sxxujin <sujn1025@naver.com>
2024-04-08 19:38:01 +0900

    CatHomepage

---
+++ check_database.php
@@ -0,0 +1,34 @@
+<?php
+
+    $db = new SQLite3('cat_homepage.db');
+
+    $enteredUsername = "babo";
+    $enteredPassword = "babo1234";
+
+    // Query to check user credentials
+    $query = "SELECT * FROM users WHERE username='{$enteredUsername}' AND password='{$enteredPassword}' ";
+    $result = $db->query($query);
+
+    // Check if the entered credentials are valid
+
+    echo "your query : $query";
+    echo "<br>";
+    
+    $row = $result->fetchArray();
+
+    echo "row : $row <br>";
+    echo "<br>";
+    
+    echo "row['username'] : {$row['username']} <br>";
+    echo "row['password'] : {$row['password']} <br>";
+
+    echo "<br>";
+
+    if ($row) {
+        echo "this is on database : $enteredUsername/$enteredPassword";
+        echo $result->fetchArray();
+    }else{
+        echo "not in database : $enteredUsername/$enteredPassword";
+    }
+
+?>
---
+++ gwaja.php
@@ -0,0 +1,5 @@
+<?php
+echo "너에게 쿠키를 저장하겠습니다.";
+setcookie("user_status", "gay", time()+3600, "/");
+setcookie("race", "Asian", time()+3600, "/");
+?>
